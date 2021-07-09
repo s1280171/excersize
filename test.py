8342ade72792b7109e0b7a278b57d59d0c436d93
s1280171 <s1280171@u-aizu.ac.jp>
2021-07-09 15:11:23 +0900

    Initial revision

---
+++ test.py
@@ -0,0 +1,9 @@
+import random
+
+print("Rolling the dice...")
+num1=random.randint(1,6)
+num2=random.randint(1,6)
+num3=num1+num2
+print("Die 1: ",num1)
+print("Die 2: ",num2)
+print("Total value:",num3)
